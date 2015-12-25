beb5c12614ae14b426adc593db07fe0c5dbb337e
vonhutrong <vonhutrong@gmail.com>
2015-12-25 13:00:52 +0700

    + add class Converter have method listToString: convert list Object to String with separator is ";"

---
+++ src/dut/pinpin/Converter.java
@@ -0,0 +1,16 @@
+package dut.pinpin;
+
+import java.util.List;
+
+public class Converter {
+
+	public static String listToString(List objects) {
+		StringBuilder sb = new StringBuilder();
+		String separator = ";";
+		for (Object obj : objects) {
+			sb.append(obj.toString()).append(separator);
+		}
+		return sb.toString();
+	}
+
+}
